1c26bfde44c3b59f7b14dba3da0d15ee3256f5df
zxt47 <zhouxiaotian47@yeah.net>
2019-02-04 20:07:13 +0800

    添加RxJava配置
    如题

---
+++ src/main/java/com/xtzhou/java/rxjava/App.java
@@ -0,0 +1,13 @@
+package com.xtzhou.java.rxjava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/xtzhou/java/rxjava/netflix/demo/MapObservable.java
@@ -0,0 +1,11 @@
+package com.xtzhou.java.rxjava.netflix.demo;
+
+
+import io.reactivex.Observable;
+
+public class MapObservable {
+    public void create()
+    {
+        Observable.create()
+    }
+}

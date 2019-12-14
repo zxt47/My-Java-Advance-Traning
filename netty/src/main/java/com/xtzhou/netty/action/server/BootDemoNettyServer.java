2f9a66bf017f5d41ac8f3d961ac9b1dbfd5966bc
zxt47 <zhouxiaotian47@yeah.net>
2019-12-14 22:50:36 +0800

    上传Netty Demo 未完工

--- netty/src/main/java/com/xtzhou/netty/action/server/BootDemoNettyServer.java
+++ netty/src/main/java/com/xtzhou/netty/action/server/BootDemoNettyServer.java
@@ -1,5 +1,13 @@
 package com.xtzhou.netty.action.server;
 
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
 import org.springframework.stereotype.Component;
 
 /**
@@ -7,6 +15,23 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class BootDemoNettyServer {
+
+    public void bind(int port)
+    {
+        EventLoopGroup bossGroup=new NioEventLoopGroup();
+        EventLoopGroup workerGroup=new NioEventLoopGroup(5);
+        ServerBootstrap serverBootstrap=new ServerBootstrap().group(bossGroup,workerGroup)
+                .channel(NioServerSocketChannel.class).handler(new LoggingHandler(LogLevel.INFO));
+        serverBootstrap.option(ChannelOption.RCVBUF_ALLOCATOR,new AdaptiveRecvByteBufAllocator())
+                .childOption(ChannelOption.RCVBUF_ALLOCATOR,new AdaptiveRecvByteBufAllocator());
+        serverBootstrap.childHandler(new ChannelInitializer<NioSocketChannel>() {
+            @Override
+            protected void initChannel(NioSocketChannel ch) throws Exception {
+                ChannelPipeline pipeline = ch.pipeline();
+                pipeline.addLast(new LoggingHandler(LogLevel.INFO));
+            }
+        });
+    }
 
 
 }

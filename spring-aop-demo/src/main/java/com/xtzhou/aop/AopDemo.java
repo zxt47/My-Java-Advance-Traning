ac8ee38f03dd8d0f1174ae48332e074920dff3bb
zxt47 <zhouxiaotian47@yeah.net>
2020-09-09 21:46:00 +0800

    Aop Demo Upload

---
+++ spring-aop-demo/src/main/java/com/xtzhou/aop/AopDemo.java
@@ -0,0 +1,16 @@
+package com.xtzhou.aop;
+
+
+import java.lang.instrument.Instrumentation;
+
+/**
+ * @author Zhouxiaotian
+ * @version 1.0
+ * @date 2020/9/9 20:57
+ * @since 1.0
+ */
+public class AopDemo {
+    public void enhanceByCglib() {
+
+    }
+}
---
+++ spring-aop-demo/src/main/java/com/xtzhou/aop/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * AOP技术和字节码操纵技术
+ */
+package com.xtzhou.aop;
